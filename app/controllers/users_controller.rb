4d69b6ae6aebffca3b9c11f82f0c68165cb72da5
Ravindra Mohith (Mac-M4) <ravindramohith@gmail.com>
2025-05-28 23:32:35 +0530

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,14 @@ class UsersController < ApplicationController
   def create
     # byebug
     if params[:password] != params[:password_confirmation]
-      redirect_to '/signup'
+      render 'new'
     end
     @user = User.create(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to articles_path
     else
-      redirect_to '/signup'
+      render 'new'
     end
   end
 
@@ -49,7 +50,7 @@ class UsersController < ApplicationController
   end
 
   def login
-    
+
   end
 
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def current_user
+    @curr ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  def logged_in?
+    !!current_user
+  end
 end
