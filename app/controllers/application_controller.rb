0b8b13a31d7d2c77ec1ab38f2068675e6adea7ec
Ravindra Mohith (Mac-M4) <ravindramohith@gmail.com>
2025-05-28 23:51:13 +0530

    helper methods modify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+  
   def hello
     render html: 'hello'
+  end
+
+  def current_user
+    @curr ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  def logged_in?
+    !!current_user
+  end
+
+  private
+
+  def require_user
+    unless logged_in?
+      flash[:alert] = "You must be logged in to perform that action."
+      redirect_to login_path
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,3 @@
 module ApplicationHelper
-  def current_user
-    @curr ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-  def logged_in?
-    !!current_user
-  end
+  
 end
