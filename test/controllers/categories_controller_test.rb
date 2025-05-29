9bcc7ead729ec66dc5c12ee8eb684e3886169db8
Ravindra Mohith (Mac-M4) <ravindramohith@gmail.com>
2025-05-29 12:50:37 +0530

    integration_test: init

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -28,7 +28,6 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  
   # test "should get edit" do
   #   get edit_category_url(@category)
   #   assert_response :success
