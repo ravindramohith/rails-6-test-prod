35e3195ffb2cb1d78a1829dc6e13b7ba17de1fef
Ravindra Mohith (Mac-M4) <ravindramohith@gmail.com>
2025-05-29 12:48:47 +0530

    category functional tests: create

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,6 +8,22 @@ class CategoriesController < ApplicationController
   end
 
   def new 
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
     
+    if @category.save
+      redirect_to @category, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
